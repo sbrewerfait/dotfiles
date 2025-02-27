4620791870e377dd5923094cbc41478e318fff88
sbrewerfait <sbrewer@faitco.com>
2025-02-27 13:33:29 -0600

    New nvim stuff

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,22 @@
 local wezterm = require("wezterm")
 
-local config = wezterm.config_builder()
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+local target = wezterm.target_triple
+
+if target:find("x86_64") then
+	config.color_scheme = "Chalk (dark) (terminal.sexy)"
+else
+	-- config.color_scheme = "Chalk (dark) (terminal.sexy)"
+	config.color_scheme = "Nocturnal Winter"
+	-- config.color_scheme = "Cyberpunk Scarlet Protocol"
+end
 
 config.font = wezterm.font("JetBrains Mono")
--- config.color_scheme = "Cyberpunk Scarlet Protocol"
-config.color_scheme = "Nocturnal Winter"
 
 config.hide_tab_bar_if_only_one_tab = true
 
---
+++ x64-nvim/lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<leader>rt", ":retab<CR>i<ESC>")
